1e509b8a042f495898fd4522dfc0305776d55044
srinivasan1609 <34212984+srinivasan1609@users.noreply.github.com>
2020-04-22 21:51:26 +0530

    refs #000 Intial Commit

---
+++ src/main/java/com/food/table/FoodTableApplication.java
@@ -0,0 +1,13 @@
+package com.food.table;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodTableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodTableApplication.class, args);
+    }
+
+}
