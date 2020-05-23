66cec26e2787b80a5b04915137aba23f652ab912
dhrn <14145706+dhrn@users.noreply.github.com>
2020-05-24 01:05:28 +0530

    * swagger fixed (#50)

--- src/main/java/com/food/table/config/SpringFoxConfig.java
+++ src/main/java/com/food/table/config/SpringFoxConfig.java
@@ -2,9 +2,14 @@ package com.food.table.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -13,7 +18,8 @@ import java.util.Arrays;
 
 @Configuration
 @EnableSwagger2
-public class SpringFoxConfig {                                    
+public class SpringFoxConfig implements WebMvcConfigurer {
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -21,6 +27,7 @@ public class SpringFoxConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
+                .apiInfo(metaData())
                 .securitySchemes(Arrays.asList(apiKey()));
     }
 
@@ -28,4 +35,22 @@ public class SpringFoxConfig {
         return new ApiKey("accessToken", "Authorization", "header");
     }
 
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("CountryBroot REST API")
+                .description("CountryBroot REST API docs")
+                .version("1.0.0")
+                .contact(new Contact("CountryBroot", "http://countrybroot.com/", "brootcountry@gmail.com"))
+                .build();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
 }
