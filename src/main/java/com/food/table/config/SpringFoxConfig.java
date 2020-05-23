d28245dabd4406af47f29c9b8b224ae10d49789f
dhrn <14145706+dhrn@users.noreply.github.com>
2020-05-23 23:31:26 +0530

    improved build (#35)
    * improved build
    
    * * removed default action
    
    * * removed default action
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * improve build
    
    * * removed default action
    
    * * removed default action
    
    * Update main.yml
    
    * Update main.yml
    
    * improve build
    
    * Merge branch 'dev-dhrn-improved-build' of https://github.com/Country-Broot/country-broot-backend into dev-dhrn-improved-build
    
    # Conflicts:
    #	.github/workflows/main.yml
    
    * fixed path
    
    * fixed path
    
    * * improved build
    
    * * improved build
    
    * * improved build
    
    * added rever proxy fix
    
    * Update pom.xml
    
    * Update main.yml

--- src/main/java/com/food/table/config/SpringFoxConfig.java
+++ src/main/java/com/food/table/config/SpringFoxConfig.java
@@ -1,5 +1,6 @@
 package com.food.table.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
@@ -13,10 +14,14 @@ import java.util.Arrays;
 
 @Configuration
 @EnableSwagger2
-public class SpringFoxConfig {                                    
+public class SpringFoxConfig {
+    @Value("${hostname}")
+    String hostname;
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .host(hostname)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
