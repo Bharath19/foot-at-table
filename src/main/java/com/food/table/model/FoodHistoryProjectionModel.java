69e6adaadf5dd59a635c271163abd62a0d4e4d14
Bharath <36304224+Bharath19@users.noreply.github.com>
2020-05-24 19:48:29 +0530

    Order API: Food History aka Menu movement API enhancement  (#56)
    * Order: Order creation improved and Food history endpoint added
    
    * Order: merge conflict fixed
    
    * Order: update cart status based on order status
    
    * Order: Swagger doc updated and order state updated in cart table
    
    * Order: Food history search issue fixed
    
    * Order: Revenue detail end point added
    
    * Order: Merge conflit issue fixed
    
    * Order: ongoing order by order type :initial commit
    
    * Order: get order details by order type and restaurantTableId
    
    * Order: Get order details by filter
    
    * Order: Pageable issue fixed
    
    * Order: add more food item API added and user model added
    
    * Order: Menu movement issue fixed
    
    * Order: order, cart state api added and Filters enhanced
    
    * Order: food tag param added in Food history API
    
    * Order: merge conflict fixed

--- src/main/java/com/food/table/model/FoodHistoryProjectionModel.java
+++ src/main/java/com/food/table/model/FoodHistoryProjectionModel.java
@@ -1,8 +1,23 @@
 package com.food.table.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public interface FoodHistoryProjectionModel {
 	public int getFoodId();
+
 	public String getFoodName();
+
 	public String getFoodImageUrl();
+
 	public int getcount();
+
+	@JsonIgnore
+	public String getFoodTagList();
+
+	default public String[] getFoodTags() {
+		if (this.getFoodTagList() != null) {
+			return getFoodTagList().split(",");
+		}
+		return null;
+	}
 }
--- src/main/java/com/food/table/repo/CartRepository.java
+++ src/main/java/com/food/table/repo/CartRepository.java
@@ -14,7 +14,7 @@ public interface CartRepository extends JpaRepository<Cart, Integer> {
 
 	Cart findByIdAndOrderId(int id, int order_id);
 
-	@Query(value = "SELECT c.food_id as foodId, f.name as foodName, f.image_url as foodImageUrl, count(*) as count FROM carts c inner join foods f on c.food_id = f.id where c.order_status= :orderStatus and DATE(c.created_at)= DATE(:orderDate) group by (c.food_id)", nativeQuery = true)
+	@Query(value = "SELECT c.food_id as foodId, f.name as foodName, f.image_url as foodImageUrl, count(*) as count, (select group_concat(ft.name) from food_tag_map fm inner join food_tag ft on ft.id = fm.food_tag_id where fm.food_id = f.id) as foodTagList FROM carts c inner join foods f on c.food_id = f.id where c.order_status= :orderStatus and DATE(c.created_at)= DATE(:orderDate) group by (c.food_id)", nativeQuery = true)
 	List<FoodHistoryProjectionModel> findByOrderStatusAndUpdatedAt(String orderStatus, String orderDate);
 
 }
