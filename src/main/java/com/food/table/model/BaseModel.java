7513571c2349474d7c07300a2727ca339ad454f4
Bharath <36304224+Bharath19@users.noreply.github.com>
2020-06-22 06:37:43 +0000

    Fix: serializer issue in Base model (#155)
    * Fix: serializer issue
    
    * Fix: serialization issue in base model

--- src/main/java/com/food/table/model/BaseModel.java
+++ src/main/java/com/food/table/model/BaseModel.java
@@ -1,11 +1,15 @@
 package com.food.table.model;
 
+import java.io.Serializable;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
-public class BaseModel {
+public class BaseModel implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	private int id;
 	
